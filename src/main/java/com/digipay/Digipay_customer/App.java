fc2e6b429df52d6c456442c4629963cec46e6ff5
SujataSharma <SujataSharma@SujataSharma>
2022-06-01 11:35:49 +0530

    This is commit for branch X

---
+++ src/main/java/com/digipay/Digipay_customer/App.java
@@ -0,0 +1,7 @@
+package com.digipay.Digipay_customer;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/digipay/Digipay_customer/AppTest.java
@@ -0,0 +1,13 @@
+package com.digipay.Digipay_customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
