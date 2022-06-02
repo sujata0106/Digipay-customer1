af68d68477d90e16b7d595eba1017dc7dcbb7c2a
SujataSharma <SujataSharma@SujataSharma>
2022-06-02 13:06:14 +0530

    new class added

---
+++ src/test/java/com/digipay/Digipay_customer/Testgit.java
@@ -0,0 +1,10 @@
+package com.digipay.Digipay_customer;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("testing commting");
+	}
+
+}
